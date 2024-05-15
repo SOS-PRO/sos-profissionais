624a10913824284c09a78010388bced537f3f407
Jonas Rafael Duarte dos Santos <jonasrdsantos@gmail.com>
2024-05-15 20:19:26 -0300

    run build on ubuntu runner

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -8,8 +8,8 @@ export const Modal = ({ isOpen, onClose, children }: ModalProps) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 w-full h-full">
-      <div className="bg-white p-2 lg:rounded-lg shadow-lg w-full max-w-7xl max-h-full overflow-y-auto m-0 lg:m-8 h-full sm:h-auto">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+      <div className="bg-white p-2 lg:rounded-lg shadow-lg w-full max-w-7xl max-h-full overflow-y-auto m-0 lg:m-8 h-full lg:h-auto">
         <div className="flex justify-end">
           <button className="text-gray-500 hover:text-gray-700" onClick={onClose}>
             X
